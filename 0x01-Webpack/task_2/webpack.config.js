d75eb7b3d2806874427c1f34da4501889cd77728
mareizaa <marce.3210@gmail.com>
2022-05-18 21:35:07 -0500

    task 2

---
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: './js/dashboard_main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            { 
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                          bypassOnDebug: true, // webpack@1.x
+                          disable: true, // webpack@2.x and newer
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+};
